8dcb6485f10e2123bbd106898bae8548e82696a4
mary-deca <maryetokwudo@yahoo.com>
2021-01-01 18:00:24 +0100

    add scroll-to-top button

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// MENU
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
 	document.querySelector('.container').classList.toggle('change');
+});
+
+// SCROLL TO TOP
+document.querySelector('.scroll-btn').addEventListener('click', () => {
+  document.querySelector('html').style.scrollBehavior = 'smooth';
+  setTimeout(() => {
+    document.querySelector('html').style.scrollBehavior = 'unset';
+  }, 1000)
 });
